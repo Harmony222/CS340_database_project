a3b942e1356b52e2e0c2c78334242e558970cffe
harmony222 <scarleth@oregonstate.edu>
2020-12-08 14:16:30 -0800

    update with heroku db info

--- db_credentials.py
+++ db_credentials.py
@@ -1,4 +1,4 @@
-host = 'localhost'
-user = 'harmonyjo' 
-passwd = 'xxx' 
-db = 'BookClubs' 
+host = 'us-cdbr-east-02.cleardb.com'
+user = 'bc8c86007a40d6' 
+passwd = 'e7492473' 
+db = 'heroku_739654fb67e2287' 
